7f0120bee19e13e80c4010a4d4258f787d6dd22c
TilakMaddy <tilak.madichetti@gmail.com>
2026-05-28 11:39:34 +0530

    c

---
+++ doc.go
@@ -0,0 +1,5 @@
+// Package mycreproject is the root of a Chainlink CRE workflow project.
+// The workflow source lives in ./cre and is compiled to WebAssembly with:
+//
+//	GOOS=wasip1 GOARCH=wasm go build -o cre/workflow.wasm ./cre
+package mycreproject
